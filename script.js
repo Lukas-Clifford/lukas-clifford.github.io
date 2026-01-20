66d3e2bcb954313789e93c67c315ff3be60dff49
Lukas Clifford <lukas.vogdt@gmail.com>
2026-01-20 12:16:34 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Seleccionar el botón
-const boton = document.getElementById("boton");
-let contenedor = document.getElementById("contenedor");
-
-// Agregar un evento al botón
-boton.addEventListener("click", () => {
-    contenedor.innerHTML += "<p>¡Hola, mundo!</p>";
-});
